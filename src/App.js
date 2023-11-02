36d90bdb2ab27d77ce0fb2ab289abd3f7181ec0b
Victor Nkire <nkirevictor77@gmail.com>
2023-11-02 17:52:27 +0100

    todo app integrated with the Queue data structure

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ function App() {
     setModal(true);
   };
 
-  let hour = new Date().getHours();
-  
   const setTime = () => {
+    let hour = new Date().getHours();
     if (hour >= 0 && hour < 12) {
       setGreeting("Good Morning!!");
     } else if (hour >= 12 && hour < 17) {
@@ -24,32 +23,36 @@ function App() {
     }
   };
 
-  useEffect(() => {
+  setInterval(() => {
     setTime();
-  }, [hour]);
+  }, 2000);
 
   return (
-    <main className='main'>
-      <div className='body-container'>
-        <img src={profile} alt='profile' />
-        <h3>{greeting}</h3>
-        <p>{new Date().toString().slice(0, 15)}</p>
-        <div className='search-bar'>
-          <input type='text' placeholder='Search' />
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            className='bi bi-search'
-            viewBox='0 0 16 16'
-          >
-            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-          </svg>
+    <>
+      <main className='main'>
+        <div className='body-container'>
+          <img src={profile} alt='profile' />
+          <h3>{greeting}</h3>
+          <p>{new Date().toString().slice(0, 15)}</p>
+          <div className='search-bar'>
+            <input type='text' placeholder='Search' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='currentColor'
+              className='bi bi-search'
+              viewBox='0 0 16 16'
+            >
+              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+            </svg>
+          </div>
+          <div>
+            <NewTasks />
+          </div>
+          <Modal modal={modal} setModal={setModal} />
         </div>
-        <NewTasks />
-        <Modal modal={modal} setModal={setModal} />
-      </div>
+      </main>
       <footer>
         <div className='addTasksBtn' onClick={openModal}>
           <svg
@@ -110,7 +113,7 @@ function App() {
           </svg>
         </div>
       </footer>
-    </main>
+    </>
   );
 }
 
